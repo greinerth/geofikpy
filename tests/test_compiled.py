46b7ad2381c55a301a4726d62b860863176f591d
Gerhard Reinerth <g.reinerth@tum.de>
2026-04-25 13:00:32 +0200

    (update): included first ik test, not working yet

--- tests/test_compiled.py
+++ tests/test_compiled.py
@@ -12,3 +12,11 @@ def test_fk() -> None:
     out = geofik.franka_fk(test)
     assert out.shape[0] == 4
     assert out.shape[1] == 4
+
+def test_ik() -> None:
+    """Test inverse kinematics
+    """
+    q = np.array([0.0, -np.pi / 4.0, 0.0, -3 * np.pi / 4.0, 0.0, np.pi / 2.0,  np.pi / 4.0])
+    desired = geofik.franka_fk(q)
+    out = geofik.franka_ik_q7(desired[:3, -1], desired[:3, :3], 0.0)
+    np.testing.assert_allclose(desired, out)
