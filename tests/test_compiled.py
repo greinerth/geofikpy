b27894a946590deaa1765200b313a9c9d6e3f002
Gerhard Reinerth <g.reinerth@tum.de>
2026-04-27 01:52:50 +0200

    (update): fixing docs, extending unit tests

--- tests/test_compiled.py
+++ tests/test_compiled.py
@@ -3,7 +3,20 @@ from __future__ import annotations
 import numpy as np
 import pytest
 
-from geofikpy import geofik
+from geofikpy import (
+    J_from_q,
+    J_to_q,
+    franka_fk,
+    franka_ik_q4,
+    franka_ik_q6,
+    franka_ik_q7,
+    franka_ik_swivel,
+    franka_J_ik_q4,
+    franka_J_ik_q6,
+    franka_J_ik_q7,
+    franka_J_ik_swivel,
+    franka_swivel,
+)
 
 
 @pytest.fixture
@@ -16,7 +29,7 @@ def get_q() -> np.ndarray[float]:
 def test_fk(get_q) -> None:
     """Test forward kinematics"""
     test = get_q
-    out = geofik.franka_fk(test)
+    out = franka_fk(test)
     assert out.shape[0] == 4
     assert out.shape[1] == 4
 
@@ -24,17 +37,15 @@ def test_fk(get_q) -> None:
 def test_ik(get_q) -> None:
     """Test inverse kinematics"""
     q = get_q
-    desired = geofik.franka_fk(q)
+    desired = franka_fk(q)
 
-    _sols, idx = geofik.franka_ik_q7(
-        desired[:3, -1], np.ravel(desired[:3, :3]), np.pi / 4.0
-    )
+    _sols, idx = franka_ik_q7(desired[:3, -1], np.ravel(desired[:3, :3]), np.pi / 4.0)
     _sols = _sols[:idx]
 
     sols = np.array([sol for sol in _sols if not np.any(np.isnan(sol))])
 
     for sol in sols:
-        out = geofik.franka_fk(sol)
+        out = franka_fk(sol)
         (
             np.testing.assert_allclose(out[:3, :3], desired[:3, :3], atol=1e-6),
             f"Invalid rotation. Expected {desired[:3, :3]}, got {out[:3, :3]}!",
@@ -44,13 +55,11 @@ def test_ik(get_q) -> None:
             f"Invalid translation. Expected {desired[:3, -1]}, got {out[:3, -1]}!",
         )
 
-    _sols, idx = geofik.franka_ik_q6(
-        desired[:3, -1], np.ravel(desired[:3, :3]), np.pi / 2.0
-    )
+    _sols, idx = franka_ik_q6(desired[:3, -1], np.ravel(desired[:3, :3]), np.pi / 2.0)
     sols = np.array([sol for sol in _sols if not np.any(np.isnan(sol))])
 
     for sol in sols:
-        out = geofik.franka_fk(sol)
+        out = franka_fk(sol)
         (
             np.testing.assert_allclose(out[:3, :3], desired[:3, :3], atol=1e-6),
             f"Invalid rotation. Expected {desired[:3, :3]}, got {out[:3, :3]}!",
@@ -60,13 +69,13 @@ def test_ik(get_q) -> None:
             f"Invalid translation. Expected {desired[:3, -1]}, got {out[:3, -1]}!",
         )
 
-    _sols, idx = geofik.franka_ik_q4(
+    _sols, idx = franka_ik_q4(
         desired[:3, -1], np.ravel(desired[:3, :3]), -3 * np.pi / 4.0
     )
     sols = np.array([sol for sol in _sols if not np.any(np.isnan(sol))])
 
     for sol in sols:
-        out = geofik.franka_fk(sol)
+        out = franka_fk(sol)
         (
             np.testing.assert_allclose(out[:3, :3], desired[:3, :3], atol=1e-6),
             f"Invalid rotation. Expected {desired[:3, :3]}, got {out[:3, :3]}!",
@@ -76,15 +85,13 @@ def test_ik(get_q) -> None:
             f"Invalid translation. Expected {desired[:3, -1]}, got {out[:3, -1]}!",
         )
 
-    theta = geofik.franka_swivel(q)
+    theta = franka_swivel(q)
 
-    _sols, idx = geofik.franka_ik_swivel(
-        desired[:3, -1], np.ravel(desired[:3, :3]), theta
-    )
+    _sols, idx = franka_ik_swivel(desired[:3, -1], np.ravel(desired[:3, :3]), theta)
     sols = np.array([sol for sol in _sols if not np.any(np.isnan(sol))])
 
     for sol in sols:
-        out = geofik.franka_fk(sol)
+        out = franka_fk(sol)
         (
             np.testing.assert_allclose(out[:3, :3], desired[:3, :3], atol=1e-6),
             f"Invalid rotation. Expected {desired[:3, :3]}, got {out[:3, :3]}!",
@@ -98,8 +105,8 @@ def test_ik(get_q) -> None:
 def test_j_ik(get_q) -> None:
     """Test jacobian inverse kinematics"""
     q = get_q
-    desired = geofik.franka_fk(q)
-    _jacs, _configs, idx = geofik.franka_J_ik_q4(
+    desired = franka_fk(q)
+    _jacs, _configs, idx = franka_J_ik_q4(
         desired[:3, -1], np.ravel(desired[:3, :3]), -3 * np.pi / 4.0, joint_angles=True
     )
     jacs = np.array([jac for jac in _jacs[:idx] if not np.any(np.isnan(jac))])
@@ -117,7 +124,7 @@ def test_j_ik(get_q) -> None:
     assert configs.shape[1] == 7
 
     for sol in configs:
-        out = geofik.franka_fk(sol)
+        out = franka_fk(sol)
         (
             np.testing.assert_allclose(out[:3, :3], desired[:3, :3], atol=1e-6),
             f"Invalid rotation. Expected {desired[:3, :3]}, got {out[:3, :3]}!",
@@ -127,7 +134,7 @@ def test_j_ik(get_q) -> None:
             f"Invalid translation. Expected {desired[:3, -1]}, got {out[:3, -1]}!",
         )
 
-    _jacs, _configs, idx = geofik.franka_J_ik_q6(
+    _jacs, _configs, idx = franka_J_ik_q6(
         desired[:3, -1], np.ravel(desired[:3, :3]), np.pi / 2.0, joint_angles=True
     )
 
@@ -141,7 +148,7 @@ def test_j_ik(get_q) -> None:
     assert configs.shape[1] == 7
 
     for sol in configs:
-        out = geofik.franka_fk(sol)
+        out = franka_fk(sol)
         (
             np.testing.assert_allclose(out[:3, :3], desired[:3, :3], atol=1e-6),
             f"Invalid rotation. Expected {desired[:3, :3]}, got {out[:3, :3]}!",
@@ -151,7 +158,7 @@ def test_j_ik(get_q) -> None:
             f"Invalid translation. Expected {desired[:3, -1]}, got {out[:3, -1]}!",
         )
 
-    _jacs, _configs, idx = geofik.franka_J_ik_q4(
+    _jacs, _configs, idx = franka_J_ik_q4(
         desired[:3, -1], np.ravel(desired[:3, :3]), -3 * np.pi / 4.0, joint_angles=True
     )
 
@@ -165,7 +172,7 @@ def test_j_ik(get_q) -> None:
     assert configs.shape[1] == 7
 
     for sol in configs:
-        out = geofik.franka_fk(sol)
+        out = franka_fk(sol)
         (
             np.testing.assert_allclose(out[:3, :3], desired[:3, :3], atol=1e-6),
             f"Invalid rotation. Expected {desired[:3, :3]}, got {out[:3, :3]}!",
@@ -175,9 +182,9 @@ def test_j_ik(get_q) -> None:
             f"Invalid translation. Expected {desired[:3, -1]}, got {out[:3, -1]}!",
         )
 
-    theta = geofik.franka_swivel(q)
+    theta = franka_swivel(q)
 
-    _jacs, _configs, idx = geofik.franka_J_ik_swivel(
+    _jacs, _configs, idx = franka_J_ik_swivel(
         desired[:3, -1], np.ravel(desired[:3, :3]), theta, joint_angles=True
     )
 
@@ -191,7 +198,7 @@ def test_j_ik(get_q) -> None:
     assert configs.shape[1] == 7
 
     for sol in configs:
-        out = geofik.franka_fk(sol)
+        out = franka_fk(sol)
         (
             np.testing.assert_allclose(out[:3, :3], desired[:3, :3], atol=1e-6),
             f"Invalid rotation. Expected {desired[:3, :3]}, got {out[:3, :3]}!",
@@ -201,22 +208,41 @@ def test_j_ik(get_q) -> None:
             f"Invalid translation. Expected {desired[:3, -1]}, got {out[:3, -1]}!",
         )
 
-    out = geofik.franka_J_ik_q4(
+    out = franka_J_ik_q4(
         desired[:3, -1], np.ravel(desired[:3, :3]), -3 * np.pi / 4, joint_angles=False
     )
     assert len(out) == 2
 
-    out = geofik.franka_J_ik_q6(
+    out = franka_J_ik_q6(
         desired[:3, -1], np.ravel(desired[:3, :3]), np.pi / 2, joint_angles=False
     )
     assert len(out) == 2
 
-    out = geofik.franka_J_ik_q7(
+    out = franka_J_ik_q7(
         desired[:3, -1], np.ravel(desired[:3, :3]), np.pi / 4, joint_angles=False
     )
     assert len(out) == 2
 
-    out = geofik.franka_J_ik_swivel(
+    out = franka_J_ik_swivel(
         desired[:3, -1], np.ravel(desired[:3, :3]), theta, joint_angles=False
     )
     assert len(out) == 2
+
+
+def test_j_to_q(get_q) -> None:
+    """Test Jacobian to joint angles"""
+    q = get_q
+    desired = franka_fk(q)
+    jac, qsol, _ = franka_J_ik_q4(
+        desired[:3, -1], np.ravel(desired[:3, :3]), -3 * np.pi / 4.0, joint_angles=True
+    )
+
+    ok_idx = np.array([i for i in range(len(qsol)) if not np.any(np.isnan(qsol[i]))])
+    qsols = np.array(qsol)[ok_idx]
+    jacs = np.array(jac)[ok_idx]
+
+    new_jacs = np.array(J_from_q(qsols[0]))
+    np.testing.assert_allclose(new_jacs, jacs[0], atol=1e-6)
+
+    qout = J_to_q(jacs[0], desired[:3, :3], "E")
+    np.testing.assert_allclose(qout, qsols[0], atol=1e-6)
